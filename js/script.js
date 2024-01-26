e60e7fd3d88847124235d25ee97603db9711739d
Polvere243 <danielercoli@gmail.com>
2024-01-27 00:08:43 +0100

    created checkbox that changes the value of a task key, following the lesson

--- js/script.js
+++ js/script.js
@@ -8,19 +8,40 @@ const app = createApp ({
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
-        ]
+        ],
+        newTaskText: ""
     }),
     computed: {
-        isTaskDone() {
-            this.task.done === true
-        }
+        
     },
     methods: {
+        // metodo per cancellare i singoli task 
         deleteTask(id) {
-           const filteredTasks = this.tasks.filter(task => id !== task.id
-           )
+           const filteredTasks = this.tasks.filter(task => id !== task.id)
            this.tasks = filteredTasks;
+        },
+        // metodo per creare un nuovo oggetto task e aggiungerlo all'array
+        addNewTask() {
+            if(!this.newTaskText) return;
+            const newTask= {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTaskText 
+            }
+            this.tasks.push(newTask);
+            this.newTaskText = "";
+        },
+        // metodo per invertire il booleano delle proprietà "done"
+        toggleTaskDone(id) {
+            this.tasks.forEach( task => {
+                if (id === task.id) {
+                    task.done = !task.done;
+                } 
+            });
+
         }
+
+    
     }
 })
 
