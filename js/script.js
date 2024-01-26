c37b2baff13c10a1f024f1c1a117df2c5787d18d
Polvere243 <danielercoli@gmail.com>
2024-01-26 12:02:58 +0100

    deleted tasks by following video lesson

--- js/script.js
+++ js/script.js
@@ -2,9 +2,24 @@ console.log("Vue Ok", Vue);
 
 const {createApp} = Vue;
 const app = createApp ({
-    data() { 
-        return {
-            tasks
+    data: () => ({ 
+        tasks: [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ]
+    }),
+    computed: {
+        isTaskDone(id) {
+            this.task.done === true
+        }
+    },
+    methods: {
+        deleteTask(id) {
+           const filteredTasks = this.tasks.filter(task => id !== task.id
+           )
+           this.tasks = filteredTasks;
         }
     }
 })
